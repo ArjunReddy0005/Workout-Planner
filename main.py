71dedf13d4e78ced76973143f9db107b37dfb9ea
Sai Arjun Reddy <your-email@example.com>
2025-01-18 00:55:22 +1100

    set up menu option

---
+++ main.py
@@ -0,0 +1,12 @@
+""" Workout Planner
+
+This personal workout planner tracks workouts, progress and body training.
+An organised plan determines which muscles are working out and if my efforts
+are alligned with my fitness goals and muscle gain.
+"""
+__author__ = "Sai Arjun Reddy"
+__docformat__ = 'reStructuredText'
+
+import json
+from datetime import datetime
+
